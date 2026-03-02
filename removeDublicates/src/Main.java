124e35486f3f78613052d5adc4112854a6fc2495
bibop <165471701+asuslava@users.noreply.github.com>
2026-03-02 20:02:00 +0100

    Initial commit

--- removeDublicates/src/Main.java
+++ removeDublicates/src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        int i = 1;
-        int len = arr.length;
+        int[] nums = {1,1,2,2,3};
+        int i = 0;
 
-        for (int j = 1; j < len; j++) {
-            if (arr[j] != arr[i-1]) {
-                arr[i] = arr[j];
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
                 i++;
+                nums[i] = nums[j];
             }
         }
-        System.out.println(i);
 
+        int newLength = i+1;
+
+        for (int k = 0; k < newLength; k++) {
+            System.out.println(nums[k]);
+        }
     }
 }
