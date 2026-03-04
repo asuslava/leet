7b36e663145cd4f248fe417c3332b4563f575342
bibop <165471701+asuslava@users.noreply.github.com>
2026-03-04 14:32:14 +0100

    Initial commit

--- plusOne/src/Main.java
+++ plusOne/src/Main.java
@@ -4,11 +4,15 @@ public class Main {
         int[] arr = {1, 2, 3};
         int one = 1;
 
-        for (int i = arr.length-1; i >= 0; i--) {
-            int sum = arr[i] + one;
-            arr[i] = sum % 10;
-            one = sum / 10;
+        for(int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] != 9) {
+                arr[i] += 1;
+                //return arr;
+            }
+            arr[i] = 0;
         }
+        arr = new int[arr.length + 1];
+        arr[0] = 1;
 
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
