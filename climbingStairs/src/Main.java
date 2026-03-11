2c9030c1454820b269c3b1a6a787db9582e5ec19
bibop <165471701+asuslava@users.noreply.github.com>
2026-03-11 19:48:13 +0100

    Initial commit

---
+++ climbingStairs/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int stairs = 5;
+        int combination = 1;
+        int step = 0;
+
+
+        for (int i = 1; i <= stairs; i++) {
+            int previousCombination = combination;
+            combination = step + previousCombination;
+            step = previousCombination;
+        }
+        System.out.println(combination);
+
+    }
+}
